32f83221ff7d0f1a9282663ca288150a71b59611
rizkhal <lamaaurizkhal@gmail.com>
2023-07-16 08:50:27 +0700

    feat: service worker

---
+++ src/sw.js
@@ -0,0 +1,8 @@
+import { precacheAndRoute } from "workbox-precaching";
+
+ServiceWorkerGlobalScope.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
+});
+
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST);
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,78 @@
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default {
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico"],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Offline First",
+        short_name: "Offline First",
+        theme_color: "#1976d2",
+        background_color: "#fafafa",
+        display: "standalone",
+        icons: [
+          {
+            src: "assets/logo.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "assets/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 };
